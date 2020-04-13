4cce42c21f16c3f6ccb03f2d78ab4485949ef7d0
TravisNutting <tnutting88@gmail.com>
2020-04-13 19:10:55 -0400

    initial setup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/fdgRoutes';
 
 const app = express();
 const PORT = 4000;
+
+routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Node and express server running on port ${PORT}`)
